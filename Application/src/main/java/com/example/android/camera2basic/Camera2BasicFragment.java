8f17d25f258646faffa1675677c163a8962d8604
JingeTu <124538898@qq.com>
2017-01-18 23:39:27 +0800

    Save image to public directory and add to gallery.

--- Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
+++ Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
@@ -22,6 +22,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
@@ -43,9 +44,12 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v13.app.FragmentCompat;
 import android.support.v4.content.ContextCompat;
@@ -63,10 +67,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
@@ -243,7 +249,11 @@ public class Camera2BasicFragment extends Fragment
 
         @Override
         public void onImageAvailable(ImageReader reader) {
-            mBackgroundHandler.post(new ImageSaver(reader.acquireNextImage(), mFile));
+            SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+            String timestamp = s.format(new Date());
+            String baseFilename = "IMG_" + timestamp + ".jpg";
+            mFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + baseFilename);
+            mBackgroundHandler.post(new ImageSaver(reader.acquireNextImage(), mFile, getActivity().getApplicationContext()));
         }
 
     };
@@ -382,7 +392,7 @@ public class Camera2BasicFragment extends Fragment
      * @return The optimal {@code Size}, or an arbitrary one if none were big enough
      */
     private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
-            int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
+                                          int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
 
         // Collect the supported resolutions that are at least as big as the preview Surface
         List<Size> bigEnough = new ArrayList<>();
@@ -394,7 +404,7 @@ public class Camera2BasicFragment extends Fragment
             if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
                     option.getHeight() == option.getWidth() * h / w) {
                 if (option.getWidth() >= textureViewWidth &&
-                    option.getHeight() >= textureViewHeight) {
+                        option.getHeight() >= textureViewHeight) {
                     bigEnough.add(option);
                 } else {
                     notBigEnough.add(option);
@@ -434,7 +444,6 @@ public class Camera2BasicFragment extends Fragment
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mFile = new File(getActivity().getExternalFilesDir(null), "pic.jpg");
     }
 
     @Override
@@ -924,9 +933,12 @@ public class Camera2BasicFragment extends Fragment
          */
         private final File mFile;
 
-        public ImageSaver(Image image, File file) {
+        private Context mContext;
+
+        public ImageSaver(Image image, File file, Context context) {
             mImage = image;
             mFile = file;
+            mContext = context;
         }
 
         @Override
@@ -938,6 +950,17 @@ public class Camera2BasicFragment extends Fragment
             try {
                 output = new FileOutputStream(mFile);
                 output.write(bytes);
+                String filePath = mFile.getAbsoluteFile().toString();
+                ContentValues image = new ContentValues();
+                String fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+                image.put(MediaStore.Images.Media.TITLE, fileName);
+                image.put(MediaStore.Images.Media.DISPLAY_NAME, fileName);
+                image.put(MediaStore.Images.Media.DESCRIPTION, "Processed by the Camera2Basic App");
+                image.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+                image.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                image.put(MediaStore.Images.Media.ORIENTATION, 0);
+                image.put(MediaStore.Images.Media.DATA, mFile.getAbsolutePath());
+                Uri result = mContext.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, image);
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -1032,3 +1055,4 @@ public class Camera2BasicFragment extends Fragment
     }
 
 }
+
